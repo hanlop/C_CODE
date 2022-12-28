76bd91daee8f57397c4d5405d6532663568168eb
Hanlop <2717043293@qq.com>
2022-12-28 20:23:32 +0800

    练习

---
+++ 多个函数/多个函数/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+void butler(void);
+int main()
+{
+	printf("I will summon.\n");
+	butler();
+	printf("dfshjfhsjdfh \n");
+
+	return 0;
+}
+void butler(void)
+{
+	printf("Han Zhengxing is so handsome\n");
+}
