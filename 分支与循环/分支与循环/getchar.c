d0b81aff342dc3f35af20a6c0aec8db04ed94d36
HanLop <112152150+hanlop@users.noreply.github.com>
2023-01-02 22:10:32 +0800

    Add files via upload

---
+++ 分支与循环/分支与循环/getchar.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int ch = 0;
+	//ctrl+zΩ· ¯—≠ª∑
+	while ((ch = getchar()) != EOF)
+	{
+		putchar(ch);
+	}
+
+	return 0;
+}
---
+++ 分支与循环/分支与循环/学习.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int n = 1;
+	while (n <= 10)
+	{
+		printf("%d ", n);
+		n++;
+	}
+
+	return 0;
+}
